b30537c2134f046fc822769e48a762257c55d961
Shravya <159636540+shravya33@users.noreply.github.com>
2025-08-04 02:01:08 +0530

    Time: 26 ms (6.13%), Space: 44.7 MB (34.61%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()){
+            return false;
+        }
+
+        HashMap<Character, Integer> scount = new HashMap<>();
+        HashMap<Character, Integer> tcount = new HashMap<>();
+
+        int n=s.length();
+        for (int i=0; i<n;i++){
+            scount.put(s.charAt(i), scount.getOrDefault(s.charAt(i), 0)+1);
+            tcount.put(t.charAt(i), tcount.getOrDefault(t.charAt(i), 0)+1);
+        }
+
+        if(scount.equals(tcount)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
