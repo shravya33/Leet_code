a11313b5ef0a23fa703e9106dd882a98df59a006
Shravya <159636540+shravya33@users.noreply.github.com>
2025-10-03 00:44:07 +0530

    Time: 3 ms (45.45%), Space: 8.3 MB (18.18%) - LeetHub

---
+++ 3336-water-bottles-ii/3336-water-bottles-ii.c
@@ -0,0 +1,11 @@
+int maxBottlesDrunk(int numBottles, int numExchange) {
+    int ans = numBottles;
+
+    while(numBottles>=numExchange){
+        numBottles = numBottles - (numExchange-1);
+        numExchange++;
+        ans++;
+    }
+
+    return ans;
+}
