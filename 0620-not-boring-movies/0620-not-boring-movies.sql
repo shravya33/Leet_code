7a72903a947f4a20c0c330f251340257e8f24e73
Shravya <159636540+shravya33@users.noreply.github.com>
2026-09-07 01:11:06 +0530

    Time: 348 ms (90.83%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,2 +1,5 @@
-# Write your MySQL query statement below
-select * from Cinema where mod(id,2)=1 and description<>"boring" order by rating desc
+/* Write your PL/SQL query statement below */
+select * from Cinema
+where mod(id,2)<>0 
+and description<>'boring'
+order by rating desc;
